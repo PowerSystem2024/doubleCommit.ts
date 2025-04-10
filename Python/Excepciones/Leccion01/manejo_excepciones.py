346f3f7eceb1e7c6bc1a8037b390af2ad3a012c8
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-04-10 01:50:54 -0300

    actualización: clase 1 python completa [excepciones]

--- Python/Excepciones/Leccion01/manejo_excepciones.py
+++ Python/Excepciones/Leccion01/manejo_excepciones.py
@@ -15,7 +15,7 @@ try:
 except ZeroDivisionError as e:
     print(f"Error en la ejecución: {e}")
     
-# Clases
+# Excepciones PYTHON
 try:
     10/0
 except Exception as e:
@@ -24,10 +24,15 @@ except Exception as e:
 # Si agregamos una ecepción más específica como el ZeroDivisionError el programa se interrumpe para avisar el tipo de error
 # y no sigue con la ejecución como en Exception
 # En el try se asigna primero las clases de tipo de error hijas y la clase padre al final
+from NumerosIgualesException import NumerosIgualesException
 resultado = None
-a = 10
-b = 0
+# a = 10
+# b = 0
 try:
+    a = int(input("Digite el primer número: "))
+    b = int(input("Digite el segundo número: "))
+    if a == b:
+        raise NumerosIgualesException('Son números iguales')
     resultado = a / b
 except TypeError as e:
     print(f'TypeError - Ocurrió un error {type(e)}')
@@ -35,6 +40,10 @@ except ZeroDivisionError as e:
     print(f'ZeroDivisionError - Ocurrió un error {type(e)}')
 except Exception as e:
     print(f'Ocurrió de error: {type(e)}')
+else:
+    print('No se arrojó ninguna excepción')
+finally:
+    print('Ejecución de este bloque finalizada')
 
 print(f'El resultado es {resultado}')
 print('Seguimos...')
