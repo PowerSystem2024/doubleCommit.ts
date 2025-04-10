3e147b49a927618f3ab728935109792afe2f766a
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-04-10 02:19:23 -0300

    remove dep: update pakgage json

--- Javascript/ProyectoMundoPC/DispositivoEntrada.js
+++ Javascript/ProyectoMundoPC/DispositivoEntrada.js
@@ -1,26 +1,26 @@
-class DispositivoEntrada {
-    constructor(tipoEntrada, marca) {
-        this._tipoEntrada = tipoEntrada;
-        this._marca = marca;
-    }
+export class DispositivoEntrada {
+  constructor(tipoEntrada, marca) {
+    this._tipoEntrada = tipoEntrada
+    this._marca = marca
+  }
 
-    get tipoEntrada() {
-        return this._tipoEntrada;
-    }
+  get tipoEntrada() {
+    return this._tipoEntrada
+  }
 
-    set tipoEntrada(valor) {
-        this._tipoEntrada = valor;
-    }
+  set tipoEntrada(valor) {
+    this._tipoEntrada = valor
+  }
 
-    get marca() {
-        return this._marca;
-    }
+  get marca() {
+    return this._marca
+  }
 
-    set marca(valor) {
-        this._marca = valor;
-    }
+  set marca(valor) {
+    this._marca = valor
+  }
 
-    toString() {
-        return `Tipo de entrada: ${this._tipoEntrada}, Marca: ${this._marca}`;
-    }
+  toString() {
+    return `Tipo de entrada: ${this._tipoEntrada}, Marca: ${this._marca}`
+  }
 }
