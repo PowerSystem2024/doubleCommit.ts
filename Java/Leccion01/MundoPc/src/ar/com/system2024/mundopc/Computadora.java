c2fb666dd47bfd4430ca38f59847e2c5af33b516
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-04-12 19:16:44 -0300

    actualización: clase Computadora java terminada

--- Java/Leccion01/MundoPc/src/ar/com/system2024/mundopc/Computadora.java
+++ Java/Leccion01/MundoPc/src/ar/com/system2024/mundopc/Computadora.java
@@ -3,15 +3,61 @@ package ar.com.system2024.mundopc;
 public class Computadora {
     private final int idComputadora;
     private static int contadorComputadoras;
-    private String marca;
-    private String monitor;
-    private String teclado;
-    private String raton;
+    private String nombre;
+    private Monitor monitor;
+    private Teclado teclado;
+    private Raton raton;
     
-    public Computadora(String marca, String monitor, String tecaldo, String raton) {
+    private Computadora() {
         this.idComputadora = ++Computadora.contadorComputadoras;
-        this.marca = marca;
-        this.monitor = monitor;
     }
     
+    public Computadora(String nombre, Monitor monitor, Teclado teclado, Raton raton) {
+        this();
+        this.nombre = nombre;
+        this.monitor = monitor;
+        this.teclado = teclado;
+        this.raton = raton;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public void setMonitor(Monitor monitor) {
+        this.monitor = monitor;
+    }
+
+    public Teclado getTeclado() {
+        return teclado;
+    }
+
+    public void setTeclado(Teclado teclado) {
+        this.teclado = teclado;
+    }
+
+    public Raton getRaton() {
+        return raton;
+    }
+
+    public void setRaton(Raton raton) {
+        this.raton = raton;
+    }
+    
+    public int getIdComputadora() {
+        return this.idComputadora;
+    }
+
+    @Override
+    public String toString() {
+        return "Computadora{" + "idComputadora=" + idComputadora + ", nombre=" + nombre + ", monitor=" + monitor + ", teclado=" + teclado + ", raton=" + raton + '}';
+    }    
 }
