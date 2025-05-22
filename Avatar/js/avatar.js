85347f0e3a7578754d8e479e2374f35de8ddec0c
Franco Morales <francomoralew36@gmail.com>
2025-05-21 22:07:01 -0300

    Juego avatar

---
+++ Avatar/js/avatar.js
@@ -0,0 +1,6 @@
+function seleccionarPersonajeJugador(){
+    alert('SELECCIONASTE TU PERSONAJE')
+}
+
+let botonPersonajeJugador =document.getElementById('boton-personaje');
+botonPersonajeJugador-addEventListener('click', seleccionarPersonajeJugador);
