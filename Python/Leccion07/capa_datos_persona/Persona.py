c485fbdb74031f5cf6f4473a27642dce8ac6978f
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-06-03 02:01:36 -0300

    Clase 8 Parte 1 Python ✅

---
+++ Python/Leccion07/capa_datos_persona/Persona.py
@@ -0,0 +1,38 @@
+class Persona:
+    def __init__(self, id_persona, nombre, apellido, email):
+        self._id_persona = id_persona
+        self._nombre = nombre
+        self._apellido = apellido
+        self._email = email
+        
+    def __str__(self):
+        return f'''
+            Id Persona: {self._id_persona},
+            Nombre: {self._nombre},
+            Apellido: {self._apellido},
+            Email: {self._email}
+        '''
+        
+    @property
+    def id_persona(self):
+        return self._id_persona
+    
+    @id_persona.setter
+    def id_persona(self, nombre):
+        self._nombre = nombre
+        
+    @property
+    def nombre(self):
+        return self._nombre
+    
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
+    
+    @property
+    def email(self):
+        return self._email
+    
+    email.setter
+    def email(self, email):
+        self._email = email
