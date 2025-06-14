33a5325c3e16ef8082c00c856da5537796dfcaab
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-06-13 21:37:33 -0300

    Clase 10 Proyecto Listar Personas Java ✅ Completo

---
+++ Java/Leccion11/ListarPersona/src/ListadoPersonasApp.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Scanner;
+
+public class ListadoPersonasApp {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        // Definimos la lista fuera del cilco while
+        List<Persona> personas = new ArrayList<>();
+
+        // Empezamos con el menú
+        var salir = false;
+        while (!salir) {
+            mostrarMenu();
+            try {
+                salir = ejecutarOperacion(entrada, personas);
+            } catch (Exception e) {
+                System.out.println("Ocurrió un error: " + e);
+            }
+        } // Fin del ciclo while
+    } // Fin del ciclo main
+
+    private static void mostrarMenu() {
+        // Mostrar las opciones
+        System.out.print("""
+                ******** Listado de personas ********
+                1. Agregar
+                2. Listar
+                3. Salir
+                """);
+        System.out.print("Digite una de las opciones: ");
+    }
+
+    private static boolean ejecutarOperacion(Scanner entrada, List<Persona> personas) {
+        int opcion = Integer.parseInt(entrada.nextLine());
+        if (opcion == 1) {
+            System.out.println("Ingrese el nombre: ");
+            String nombre = entrada.nextLine();
+            System.out.println("Ingrese el teléfono: ");
+            String tel = entrada.nextLine();
+            System.out.println("Ingrese el email: ");
+            String email = entrada.nextLine();
+            Persona persona = new Persona(nombre, tel, email);
+            personas.add(persona);
+            System.out.println("Se añadió persona con éxito: " + persona.toString());
+            return false;
+        } else if (opcion == 2) {
+            if (personas.toArray().length == 0) {
+                System.out.println("No hay personas cargadas en el sistema para listar.");
+            }
+            System.out.println("Personas en la lista: ");
+            for (int i = 0; i < personas.toArray().length; i++) {
+                System.out.println(personas.toArray()[i].toString());
+            }
+        } else {
+            System.out.println("Saliendo de la aplicación...");
+            return true;
+        }
+        return false;
+    }
+}
--- Java/Leccion11/ListarPersona/src/Persona.java
+++ Java/Leccion11/ListarPersona/src/Persona.java
@@ -1,4 +1,67 @@
-package PACKAGE_NAME;
-
 public class Persona {
+    private int id;
+    private String nombre;
+    private String tel;
+    private String email;
+    private static int numeroDePersonas;
+
+    // Constructor vació
+    public Persona(){
+        this.id = ++Persona.numeroDePersonas;
+    }
+
+    // Constructor con parámetros (sobrecarga de constructores)
+    public Persona(String nombre, String tel, String email) {
+        this(); // Llamamos al constructor vacío
+        this.nombre = nombre;
+        this.tel = tel;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", tel='" + tel + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Juan Palote", "2664562887", "palote_juan@gmail.com");
+        System.out.println("persona1 = " + persona1);
+    }
 }
